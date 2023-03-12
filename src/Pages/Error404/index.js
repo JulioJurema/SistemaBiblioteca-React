ac61e0b3a8ea2cae4a89d5015aa58382ab52c9b5
JulioJurema <zueranever3j@gmail.com>
2023-03-12 16:41:24 -0300

    Criação página de erro

---
+++ src/Pages/Error404/index.js
@@ -0,0 +1,15 @@
+import './Style.css'
+
+function Error404() {
+    return (
+      <div className='container'>
+        <div>
+            <p className='erro'>404</p>
+            <p className='titulo'>PÁGINA NÃO ENCONTRADA</p>
+        </div>      
+      </div>
+    );
+  }
+  
+  export default Error404;
+  
--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import Home from './Pages/Home'
 import Cadastro from './Pages/Livros'
 import Usuarios from './Pages/Usuarios'
 import Navbar from './Components/NavBar'
+import Error404 from './Pages/Error404'
 
 function RoutesApp(){
     return(
@@ -17,6 +18,8 @@ function RoutesApp(){
                         <Route path="/" element={ <Home/> } />
                         <Route path="/cadastro" element={ <Cadastro/> } />
                         <Route path="/usuarios" element={ <Usuarios/> } />
+
+                        <Route path="*" element={ <Error404 /> } />
                     </Routes>
                 </div>
             </div>
